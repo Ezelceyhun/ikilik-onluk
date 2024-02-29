3600d45d2d561bb556d99cee6f80d32817aae59e
Ceyhun <47317865+Ezelceyhun@users.noreply.github.com>
2024-02-29 08:30:43 +0300

    butonlar ayrıldı ve güncelleştirildi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,6 +47,15 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,7 +73,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(765, 527);
+            this.dataGridView1.Size = new System.Drawing.Size(765, 716);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -102,9 +111,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(821, 128);
+            this.button1.Location = new System.Drawing.Point(813, 138);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 37);
+            this.button1.Size = new System.Drawing.Size(73, 47);
             this.button1.TabIndex = 5;
             this.button1.Text = "hesap";
             this.button1.UseVisualStyleBackColor = true;
@@ -122,17 +131,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(807, 265);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(808, 479);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.Size = new System.Drawing.Size(55, 16);
             this.label2.TabIndex = 7;
+            this.label2.Text = "decimal";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(936, 128);
+            this.button2.Location = new System.Drawing.Point(892, 138);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(98, 37);
+            this.button2.Size = new System.Drawing.Size(98, 47);
             this.button2.TabIndex = 8;
             this.button2.Text = "temizle";
             this.button2.UseVisualStyleBackColor = true;
@@ -151,37 +162,38 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(807, 222);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(810, 234);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(136, 25);
+            this.label4.Size = new System.Drawing.Size(101, 18);
             this.label4.TabIndex = 10;
             this.label4.Text = "Binary Yazımı:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(807, 307);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(808, 294);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(151, 25);
+            this.label5.Size = new System.Drawing.Size(114, 18);
             this.label5.TabIndex = 11;
             this.label5.Text = "Decimal Yazımı:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(807, 350);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(810, 323);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.Size = new System.Drawing.Size(55, 16);
             this.label6.TabIndex = 12;
+            this.label6.Text = "decimal";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(856, 555);
+            this.label7.Location = new System.Drawing.Point(841, 678);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(293, 25);
             this.label7.TabIndex = 13;
@@ -190,7 +202,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(812, 392);
+            this.textBox1.Location = new System.Drawing.Point(797, 515);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(222, 35);
@@ -198,11 +210,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1064, 392);
+            this.button3.Location = new System.Drawing.Point(1043, 515);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 35);
+            this.button3.Size = new System.Drawing.Size(58, 87);
             this.button3.TabIndex = 15;
-            this.button3.Text = "EKLE";
+            this.button3.Text = "Toplu Ekle";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -210,7 +222,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label8.Location = new System.Drawing.Point(807, 502);
+            this.label8.Location = new System.Drawing.Point(792, 625);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(130, 25);
             this.label8.TabIndex = 16;
@@ -220,26 +232,127 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label9.Location = new System.Drawing.Point(943, 502);
+            this.label9.Location = new System.Drawing.Point(928, 625);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(130, 25);
+            this.label9.Size = new System.Drawing.Size(144, 25);
             this.label9.TabIndex = 17;
-            this.label9.Text = "Binary Yazımı";
+            this.label9.Text = "\"Çevrilmiş Hali\"";
             // 
             // textBox2
             // 
             this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(812, 444);
+            this.textBox2.Location = new System.Drawing.Point(797, 567);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(222, 35);
             this.textBox2.TabIndex = 19;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1125, 515);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 35);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Yazma";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1125, 567);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 35);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Okuma";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(793, 202);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(129, 24);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Okuma Yetkisi";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(793, 355);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(125, 24);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Yazma Yetkisi";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(809, 266);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(44, 16);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "binary";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(808, 420);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(44, 16);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "binary";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(996, 138);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 47);
+            this.button6.TabIndex = 26;
+            this.button6.Text = "Label Temizle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label14.Location = new System.Drawing.Point(810, 450);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(114, 18);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "Decimal Yazımı:";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label15.Location = new System.Drawing.Point(812, 390);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(101, 18);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "Binary Yazımı:";
+            this.label15.Click += new System.EventHandler(this.label15_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1181, 587);
+            this.ClientSize = new System.Drawing.Size(1370, 740);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -285,6 +398,15 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -55,41 +55,61 @@ namespace json_dosya_okuma_ve_2li_sistemden_10luk_sayıya_cevirme
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            label12.Text = "";
+            label13.Text = "";
+            label6.Text = "";
+            label2.Text = "";
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
                 if ((bool)row.Cells["readAccess"].Value)
                 {
                     //seçili olanlara 1 ekle
                     label1.Text += " " + row.Index.ToString();
-                    label2.Text += 1.ToString();
+                    label12.Text += 1.ToString();
                 }
                 else if ((bool)row.Cells["writeAccess"].Value)
                 {
                     label1.Text += " " + row.Index.ToString();
-                    label2.Text += 1.ToString();
+                    label13.Text += 1.ToString();
                 }
                 else if ((bool)row.Cells["writeAccess"].Value && (bool)row.Cells["readAccess"].Value)
                 {
                     //her iki sütunda seçim yapıldıysa
-                    label7.Show();
+                    label13.Text += 1.ToString();
+                    label12.Text += 1.ToString();
                 }
                 else
                 {
                     //seçili olmayanlara 0 ekle
-                    label2.Text += 0.ToString();
+                    label12.Text += 0.ToString();
+                    label13.Text += 0.ToString();
                 }
             }
             //sayıların ters yazımı
             string cevir = "";
-            for (int a = 0; a < label2.Text.Length; a++)
+            for (int a = 0; a < label12.Text.Length; a++)
             {
-                cevir = label2.Text.Substring(a, 1) + cevir;
+                cevir = label12.Text.Substring(a, 1) + cevir;
             }
-            label2.Text = cevir;
+            label12.Text = cevir;
+            string cevir2 = "";
+            for (int a = 0; a < label13.Text.Length; a++)
+            {
+                cevir2 = label13.Text.Substring(a, 1) + cevir2;
+            }
+            label13.Text = cevir2;
         //ikilikten onluk çevirme
         tekrar:
-            string say = label2.Text;
+            string say = label12.Text;
             foreach (char basamak in say)
+            {
+                if ((basamak != '0') && (basamak != '1'))
+                {
+                    goto tekrar;
+                }
+            }
+            string say2 = label13.Text;
+            foreach (char basamak in say2)
             {
                 if ((basamak != '0') && (basamak != '1'))
                 {
@@ -103,7 +123,15 @@ namespace json_dosya_okuma_ve_2li_sistemden_10luk_sayıya_cevirme
                 onluk += (Math.Pow(2, sira) * Convert.ToInt32(say.Substring(i, 1)));
                 sira++;
             }
+            double onluk2 = 0;
+            int sira2 = 0;
+            for (int i = say2.Length - 1; i >= 0; i--)
+            {
+                onluk2 += (Math.Pow(2, sira2) * Convert.ToInt32(say2.Substring(i, 1)));
+                sira2++;
+            }
             label6.Text = onluk.ToString();
+            label2.Text = onluk2.ToString();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -180,15 +208,14 @@ namespace json_dosya_okuma_ve_2li_sistemden_10luk_sayıya_cevirme
             }
             string yeni_cevir = cevir;
             foreach (int isim in basamak)
-            {
-                
-                don:
+            {             
                     yer = yeni_cevir.ToUpper().IndexOf("1".ToUpper(), yer + 1);
                     
                     if (yer < 0)
                     {
-                        goto bitir;
-                    }
+                    MessageBox.Show("Bitti.");
+                    break;
+                }
                     else
                     {
                         textBox2.SelectionStart = yer;
@@ -199,14 +226,152 @@ namespace json_dosya_okuma_ve_2li_sistemden_10luk_sayıya_cevirme
                         bool isNeverChecked = (bool)never.EditedFormattedValue;
                         once.Value = "true";
                         never.Value = "true";
-                        MessageBox.Show((yer+1).ToString() + ". Kutu Seçildi !");
-                        goto don;                                   
+                        MessageBox.Show((yer+1).ToString() + ". Kutu Seçildi !");                                   
                     }
+                }                  
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<int> basamak = new List<int>();
+            int kalan = 0;
+            string yazikalan = "";
+            int sayi = Convert.ToInt32(textBox1.Text);
+            while (sayi != 0)
+            {
+                kalan = sayi % 2;
+                sayi = sayi / 2;
+                yazikalan = kalan.ToString() + yazikalan;
+                basamak.Add(kalan);
+            }
+            textBox2.Text = yazikalan.ToString();
+            label9.Text = yazikalan.ToString();
+            //sayı ters çevir
+            string cevir = "";
+            string cevir2 = "";
+            for (int a = 0; a < textBox2.Text.Length; a++)
+            {
+                cevir = textBox2.Text.Substring(a, 1) + cevir;
+            }
+            for (int a = 0; a < label9.Text.Length; a++)
+            {
+                cevir2 = label9.Text.Substring(a, 1) + cevir2;
+            }
+            textBox2.Text = cevir.PadLeft(32, '0');
+            label9.Text = cevir2.PadLeft(32, '0');
+            cevir = "";
+            for (int a = 0; a < textBox2.Text.Length; a++)
+            {
+                cevir = textBox2.Text.Substring(a, 1) + cevir;
+            }
+            string yeni_cevir = cevir;
+            foreach (int isim in basamak)
+            {
+                yer = yeni_cevir.ToUpper().IndexOf("1".ToUpper(), yer + 1);
+
+                if (yer < 0)
+                {
+                    MessageBox.Show("Bitti.");
+                    break;
                 }
-            
-        bitir:
-            MessageBox.Show("Bitti.");
-            
-        }        
+                else
+                {
+                    textBox2.SelectionStart = yer;
+                    textBox2.SelectionLength = "1".Length;
+                    DataGridViewCheckBoxCell never = dataGridView1.Rows[yer].Cells[2] as DataGridViewCheckBoxCell;
+                    bool isNeverChecked = (bool)never.EditedFormattedValue;
+                    never.Value = "true";
+                    MessageBox.Show((yer + 1).ToString() + ". Kutu Seçildi !");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<int> basamak = new List<int>();
+            int kalan = 0;
+            string yazikalan = "";
+            int sayi = Convert.ToInt32(textBox1.Text);
+            while (sayi != 0)
+            {
+                kalan = sayi % 2;
+                sayi = sayi / 2;
+                yazikalan = kalan.ToString() + yazikalan;
+                basamak.Add(kalan);
+            }
+            textBox2.Text = yazikalan.ToString();
+            label9.Text = yazikalan.ToString();
+            //sayı ters çevir
+            string cevir = "";
+            string cevir2 = "";
+            for (int a = 0; a < textBox2.Text.Length; a++)
+            {
+                cevir = textBox2.Text.Substring(a, 1) + cevir;
+            }
+            for (int a = 0; a < label9.Text.Length; a++)
+            {
+                cevir2 = label9.Text.Substring(a, 1) + cevir2;
+            }
+            textBox2.Text = cevir.PadLeft(32, '0');
+            label9.Text = cevir2.PadLeft(32, '0');
+            cevir = "";
+            for (int a = 0; a < textBox2.Text.Length; a++)
+            {
+                cevir = textBox2.Text.Substring(a, 1) + cevir;
+            }
+            string yeni_cevir = cevir;
+            foreach (int isim in basamak)
+            {
+                yer = yeni_cevir.ToUpper().IndexOf("1".ToUpper(), yer + 1);
+
+                if (yer < 0)
+                {
+                    MessageBox.Show("Bitti.");
+                    break;
+                }
+                else
+                {
+                    textBox2.SelectionStart = yer;
+                    textBox2.SelectionLength = "1".Length;
+                    DataGridViewCheckBoxCell once = dataGridView1.Rows[yer].Cells[3] as DataGridViewCheckBoxCell;
+                    bool isOnceChecked = (bool)once.EditedFormattedValue;
+                    once.Value = "true";
+                    MessageBox.Show((yer + 1).ToString() + ". Kutu Seçildi !");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label12.Text = "";
+            label13.Text = "";
+            label6.Text = "";
+            label2.Text = "";
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
